ab3bcc5224e36349618cd23923141b4769f5171c
Manthan Mule <mulemanthan525@gmail.com>
2026-01-08 18:42:41 +0530

    Leetcode problem no.53

---
+++ 53-Maximum_Subarray.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int currSum = 0;
+        int maxSum = INT_MIN;
+
+        for (int val : nums) {
+            currSum += val;
+            maxSum = max(maxSum, currSum);
+
+            if (currSum < 0) {
+                currSum = 0;
+            }
+        }
+        return maxSum;
+    }
+};
