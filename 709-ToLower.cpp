05158858c730fd07899ae14b1d1bfe8753c6f6e0
Manthan Mule <mulemanthan525@gmail.com>
2026-03-16 07:49:03 +0530

    Leetcode Problem 709

---
+++ 709-ToLower.cpp
@@ -0,0 +1,13 @@
+#include<iostream>;
+#include<string>;
+using namespace std;
+
+class Solution {
+public:
+    string toLowerCase(string s) {
+        for(int i = 0; i < s.length(); i++){
+            s[i] = tolower(s[i]);
+        }
+        return s;
+    }
+};
