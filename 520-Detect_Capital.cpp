995fea7a851bed8a4cb094da562dfd7b0c5d9599
Manthan Mule <mulemanthan525@gmail.com>
2026-03-26 23:34:51 +0530

    Leetcode Problem 520

---
+++ 520-Detect_Capital.cpp
@@ -0,0 +1,20 @@
+#include <string>
+using namespace std;
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int upperCount = 0;
+
+        for (char c : word) {
+            if (isupper(c)) {
+                upperCount++;
+            }
+        }
+
+        if (upperCount == word.length()) return true; 
+        if (upperCount == 0) return true;             
+        if (upperCount == 1 && isupper(word[0])) return true; 
+
+        return false;
+    }
+};
